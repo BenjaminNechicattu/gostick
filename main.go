a7525cb1f341fb0c6792974d6fda55e4dda911b2
Benjamin <benjaminnechicattu@gmail.com>
2026-05-23 23:25:26 +0530

    added tray icon

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "gostick/internal/runtime"
+import (
+	_ "embed"
+	"gostick/internal/runtime"
+	"os"
+
+	"github.com/getlantern/systray"
+)
+
+//go:embed assets/gostick.ico
+var icon []byte
 
 func main() {
-	runtime.Start()
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+
+	// Start GoStick runtime
+	go runtime.Start()
+
+	// Set tray icon
+	systray.SetIcon(icon)
+
+	// Tray metadata
+	systray.SetTitle("GoStick")
+	systray.SetTooltip("GoStick Running")
+
+	// Menu items
+	mQuit := systray.AddMenuItem(
+		"Quit",
+		"Quit GoStick",
+	)
+
+	// Quit handler
+	go func() {
+
+		<-mQuit.ClickedCh
+
+		systray.Quit()
+		os.Exit(0)
+	}()
+}
+
+func onExit() {
+
 }
